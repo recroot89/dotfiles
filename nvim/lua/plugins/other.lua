d8a547c327785f2fb709cc002645589e879c94d4
Shukhrat Rakhimov <recroot89@gmail.com>
2026-06-22 19:33:41 +0500

    Fix luarocks

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -22,7 +22,9 @@ return {
   {
     "vhyrro/luarocks.nvim",
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
+    opts = {
+      rocks = { "dkjson" },
+    },
   },
   { "isobit/vim-caddyfile" },
   { "tpope/vim-rails" },
